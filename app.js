898edc900f2c2df5c4bb539b7f95c03fce3f7725
Gokul0201 <kgokula314@gmail.com>
2022-09-11 02:02:05 +0530

    cors error

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors())
+app.use(cors({
+  origin:"*"
+}))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
